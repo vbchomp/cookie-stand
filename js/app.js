ab5535a8e13087e33e7f03f19f170c8014f804c7
Heather B <vbchomp@gmail.com>
2021-05-12 12:02:21 -0700

    created seattle list in sales.html, able to get random customers and total hours to work

--- js/app.js
+++ js/app.js
@@ -3,19 +3,40 @@
 // POL
 console.log('Wazzup');
 
+const seattleList = document.getElementById('seattle-list');
+
 let seattle = {
   nameLoc: 'Seattle',
   minCust: 23,
   maxCust: 65,
-  avgCust: 6.3,
+  avgCookie: 6.3,
+  hoursOpenArray: ['0600', '0700', '0800', '0900', '1000', '1100', '1200', '1300', '1400', '1500', '1600', '1700', '1800', '1900'],
   randomCust: function() {
     return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
-  }
+  },
   // function for switch or multi dimensional array for times/cookies
   // function for returning locations arrays
-  // sum of hourly totals for daily total
+  // hourly totals
+  totalHourly: function(){
+    for (let i = 0; i < this.hoursOpenArray.length; i++){
+      let totalHourly = Math.floor(this.randomCust() * this.avgCookie);
+      let li = document.createElement('li');
+      li.textContent = `${this.hoursOpenArray[i]}: ${totalHourly} cookies`;
+      seattleList.appendChild(li);
+      // return `${this.hoursOpenArray[i]}: ${totalHourly}`;
+    }
+  }
+  // totalDaily
+  // totalDaily: function(){
+  //   let total = 0;
+  //   for (let i = 0; i < this.hoursOpenArray.length; i++){
+  //     total = total + this.totalHourly[1];
+  //     let li = document.createElement('li');
+  //     li.textContent = `Total: ${total} cookies`;
+  //     seattleList.appendChild(li);
+  //   }
+  // }
 };
 
-console.log(seattle.randomCust());
-
-
+//console.log(seattle.randomCust());
+console.log(seattle.totalHourly());
