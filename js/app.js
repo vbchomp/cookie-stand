47b5c9636e9f6d5795e5910e1cb1bf45a87d0cc9
Heather B <vbchomp@gmail.com>
2021-05-13 21:15:12 -0700

    fixed issue from grading where cookiesperhrarray was looping. applied across all cities.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // POL
-console.log('Wazzup');
+// console.log('Wazzup');
 
 // access DOM with each container
 // const mainContainer = document.getElementsByClassName('main-conatiner');
@@ -34,62 +34,33 @@ let seattle = {
     for (let i = 0; i < hoursOpenArray.length; i++) {
       this.cookies = Math.ceil(this.avgCookieSale * this.randomCust());
       this.cookiesPerHrArray.push(this.cookies);
-      // console.log(this.cookies);
+      // adds up hourly totals to get daily totals
+      this.dailyTotal += this.cookies;
     }
-    // adds up hourly totals to get daily totals
-    this.dailyTotal += this.cookies;
-    // console.log('This is daily total: ', this.dailyTotal);
-    return this.cookiesPerHrArray;
   },
   // sends arrays to html list items
   renderToHTML: function () {
+    this.totalHourlySales();
     for (let i = 0; i < hoursOpenArray.length; i++) {
       let li = document.createElement('li');
-      li.textContent = `${hoursOpenArray[i]}: ${this.totalHourlySales()[i]} cookies`;
+      li.textContent = `${hoursOpenArray[i]}: ${this.cookiesPerHrArray[i]} cookies`;
       seattleList.appendChild(li);
     }
     seattleList.append('Total: ', this.dailyTotal);
-  // Another render function based on will's
-  // Does not recognize maincontainer - ask TA tonight
-  // renderToHTMLVerbose: function () {
-  //   this.totalHourlySales();
-  //   let elementArray = [];
-  //   for (let i = 0; i < hoursOpenArray.length; i++) {
-  //     let elementLine = `${hoursOpenArray[i]}: ${this.cookiesPerHrArray[i]} cookies`;
-  //     elementArray.push(elementLine);
-  //   }
-  //   elementArray.push(`Total: ${this.dailyTotal} cookies`);
-  //   //creating each location section to include a header and list
-  //   let citySection = document.createElement('section');
-  //   //creating each location header
-  //   let cityHeader = document.createElement('h2');
-  //   //creating each location list
-  //   let ulEl = document.createElement('ul');
-  //   //appending the elements
-  //   maincontainer.appendChild(citySection);
-  //   citySection.appendChild(cityHeader);
-  //   citySection.appendChild(ulEl);
-  //   //for loop to include the list items
-  //   for (let i = 0; i < elementArray.length; i++) {
-  //     let listEl = document.createElement('li');
-  //     listEl.textContent = elementArray[i];
-  //     ulEl.appendChild(listEl);
-  //   }
   }
 };
 seattle.renderToHTML();
-// use if using seattle.renderToHTMLVerbose()
-//seattle.renderToHTMLVerbose();
 
-// // render function from Will - need to look at more to see if i can incorporate
-// render: function () {
+// Another render function based on will's
+// Does not recognize maincontainer - ask TA tonight
+// renderToHTMLVerbose: function () {
 //   this.totalHourlySales();
-//   console.log('this is the render');
 //   let elementArray = [];
-//   for (let i =0; i < hoursOpenArray.length; i++) {
+//   for (let i = 0; i < hoursOpenArray.length; i++) {
 //     let elementLine = `${hoursOpenArray[i]}: ${this.cookiesPerHrArray[i]} cookies`;
 //     elementArray.push(elementLine);
 //   }
+//   elementArray.push(`Total: ${this.dailyTotal} cookies`);
 //   //creating each location section to include a header and list
 //   let citySection = document.createElement('section');
 //   //creating each location header
@@ -100,24 +71,15 @@ seattle.renderToHTML();
 //   maincontainer.appendChild(citySection);
 //   citySection.appendChild(cityHeader);
 //   citySection.appendChild(ulEl);
-//   //for loop to include the li
-//   for(let i = 0; i< elementArray.length; i++){
+//   //for loop to include the list items
+//   for (let i = 0; i < elementArray.length; i++) {
 //     let listEl = document.createElement('li');
 //     listEl.textContent = elementArray[i];
 //     ulEl.appendChild(listEl);
 //   }
+//   //use if using seattle.renderToHTMLVerbose()
+//   seattle.renderToHTMLVerbose();
 
-// console.log(seattle.renderToHTML());
-console.log(seattle.cookiesPerHrArray);
-
-// Table of values
-// Seattle	23	65	6.3
-// Tokyo	3	24	1.2
-// Dubai	11	38	3.7
-// Paris	20	38	2.3
-// Lima	2	16	4.6
-
-// Tokyo	3	24	1.2
 // Tokyo Site object literal
 let tokyo = {
   nameLoc: 'Tokyo',
@@ -139,26 +101,23 @@ let tokyo = {
     for (let i = 0; i < hoursOpenArray.length; i++) {
       this.cookies = Math.ceil(this.avgCookieSale * this.randomCust());
       this.cookiesPerHrArray.push(this.cookies);
-      // console.log(this.cookies);
+      // adds up hourly totals to get daily totals
+      this.dailyTotal += this.cookies;
     }
-    // adds up hourly totals to get daily totals
-    this.dailyTotal += this.cookies;
-    // console.log('This is daily total: ', this.dailyTotal);
-    return this.cookiesPerHrArray;
   },
   // sends arrays to html list items
   renderToHTML: function () {
+    this.totalHourlySales();
     for (let i = 0; i < hoursOpenArray.length; i++) {
       let li = document.createElement('li');
-      li.textContent = `${hoursOpenArray[i]}: ${this.totalHourlySales()[i]} cookies`;
+      li.textContent = `${hoursOpenArray[i]}: ${this.cookiesPerHrArray[i]} cookies`;
       tokyoList.appendChild(li);
     }
     tokyoList.append('Total: ', this.dailyTotal);
-  },
+  }
 };
 tokyo.renderToHTML();
 
-// Dubai	11	38	3.7
 // Dubai Site object literal
 let dubai = {
   nameLoc: 'Dubai',
@@ -180,26 +139,23 @@ let dubai = {
     for (let i = 0; i < hoursOpenArray.length; i++) {
       this.cookies = Math.ceil(this.avgCookieSale * this.randomCust());
       this.cookiesPerHrArray.push(this.cookies);
-      // console.log(this.cookies);
+      // adds up hourly totals to get daily totals
+      this.dailyTotal += this.cookies;
     }
-    // adds up hourly totals to get daily totals
-    this.dailyTotal += this.cookies;
-    // console.log('This is daily total: ', this.dailyTotal);
-    return this.cookiesPerHrArray;
   },
   // sends arrays to html list items
   renderToHTML: function () {
+    this.totalHourlySales();
     for (let i = 0; i < hoursOpenArray.length; i++) {
       let li = document.createElement('li');
-      li.textContent = `${hoursOpenArray[i]}: ${this.totalHourlySales()[i]} cookies`;
+      li.textContent = `${hoursOpenArray[i]}: ${this.cookiesPerHrArray[i]} cookies`;
       dubaiList.appendChild(li);
     }
     dubaiList.append('Total: ', this.dailyTotal);
-  },
+  }
 };
 dubai.renderToHTML();
 
-// Paris	20	38	2.3
 // Paris Site object literal
 let paris = {
   nameLoc: 'Paris',
@@ -221,26 +177,23 @@ let paris = {
     for (let i = 0; i < hoursOpenArray.length; i++) {
       this.cookies = Math.ceil(this.avgCookieSale * this.randomCust());
       this.cookiesPerHrArray.push(this.cookies);
-      // console.log(this.cookies);
+      // adds up hourly totals to get daily totals
+      this.dailyTotal += this.cookies;
     }
-    // adds up hourly totals to get daily totals
-    this.dailyTotal += this.cookies;
-    // console.log('This is daily total: ', this.dailyTotal);
-    return this.cookiesPerHrArray;
   },
   // sends arrays to html list items
   renderToHTML: function () {
+    this.totalHourlySales();
     for (let i = 0; i < hoursOpenArray.length; i++) {
       let li = document.createElement('li');
-      li.textContent = `${hoursOpenArray[i]}: ${this.totalHourlySales()[i]} cookies`;
+      li.textContent = `${hoursOpenArray[i]}: ${this.cookiesPerHrArray[i]} cookies`;
       parisList.appendChild(li);
     }
     parisList.append('Total: ', this.dailyTotal);
-  },
+  }
 };
 paris.renderToHTML();
 
-// Lima	2	16	4.6
 // Lima Site object literal
 let lima = {
   nameLoc: 'Lima',
@@ -262,21 +215,19 @@ let lima = {
     for (let i = 0; i < hoursOpenArray.length; i++) {
       this.cookies = Math.ceil(this.avgCookieSale * this.randomCust());
       this.cookiesPerHrArray.push(this.cookies);
-      // console.log(this.cookies);
+      // adds up hourly totals to get daily totals
+      this.dailyTotal += this.cookies;
     }
-    // adds up hourly totals to get daily totals
-    this.dailyTotal += this.cookies;
-    // console.log('This is daily total: ', this.dailyTotal);
-    return this.cookiesPerHrArray;
   },
   // sends arrays to html list items
   renderToHTML: function () {
+    this.totalHourlySales();
     for (let i = 0; i < hoursOpenArray.length; i++) {
       let li = document.createElement('li');
-      li.textContent = `${hoursOpenArray[i]}: ${this.totalHourlySales()[i]} cookies`;
+      li.textContent = `${hoursOpenArray[i]}: ${this.cookiesPerHrArray[i]} cookies`;
       limaList.appendChild(li);
     }
     limaList.append('Total: ', this.dailyTotal);
-  },
+  }
 };
 lima.renderToHTML();
